5b2833a43f62f6370d5aef25351b05ac6b189fbb
Cryostrixx <cryostrixx.dev@gmail.com>
2024-10-12 05:46:43 -0700

    Add experimental arguments support to scripts\start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -6,71 +6,114 @@
     Version        : #{replaceme}
 #>
 
+# Create a param-block containing the script's parameters.
 param (
     [switch]$Debug,
     [string]$Config,
     [switch]$Run
 )
 
-# Set DebugPreference based on the -Debug switch
+# Clear all previously displayed console output messages.
+Clear-Host
+
+# Initialize an empty array to store the script's arguments.
+$argsList = @()
+
+# Iterate over the parameters and append them to $argsList.
+$PSBoundParameters.GetEnumerator() | ForEach-Object {
+    $argsList += if ($_.Value -is [switch] -and $_.Value) {
+        "-$($_.Key)"
+    } elseif ($_.Value) {
+        "-$($_.Key) `"$($_.Value)`""
+    }
+}
+
+# Set DebugPreference based on the -Debug switch.
 if ($Debug) {
     $DebugPreference = "Continue"
 }
 
+# Handle the -Config parameter.
 if ($Config) {
     $PARAM_CONFIG = $Config
 }
 
+# Handle the -Run switch.
 $PARAM_RUN = $false
-# Handle the -Run switch
 if ($Run) {
     Write-Host "Running config file tasks..."
     $PARAM_RUN = $true
 }
 
-# Load DLLs
+# Load DLLs.
 Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName System.Windows.Forms
 
-# Variable to sync between runspaces
+# Variable to sync between runspaces.
 $sync = [Hashtable]::Synchronized(@{})
 $sync.PSScriptRoot = $PSScriptRoot
 $sync.version = "#{replaceme}"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
-    $argList = @()
+# Store the latest script URL in a variable.
+$latestScriptURL = "https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1"
 
-    $PSBoundParameters.GetEnumerator() | ForEach-Object {
-        $argList += if ($_.Value -is [switch] -and $_.Value) {
-            "-$($_.Key)"
-        } elseif ($_.Value) {
-            "-$($_.Key) `"$($_.Value)`""
-        }
-    }
+# Store the elevation status of the process.
+$isElevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
+# Check if the script is running as administrator.
+if (!$isElevated) {
+    # Let the user know the script needs to run as admin.
+    Write-Output "WinUtil needs to be run as administrator. Attempting to relaunch."
+
+    # Create a script construct and store it in-memory.
     $script = if ($MyInvocation.MyCommand.Path) {
-        "& { & '$($MyInvocation.MyCommand.Path)' $argList }"
-    } else {
-        "iex '& { $(irm https://github.com/ChrisTitusTech/winutil/releases/latest/download/winutil.ps1) } $argList'"
+        "& '" + $MyInvocation.MyCommand.Path + "' $argsList"
     }
 
-    $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
+    # Setup the processes used to launch the script.
+    $powershellCmd = if (Get-Command pwsh.exe -ErrorAction SilentlyContinue) { "pwsh.exe" } else { "powershell.exe" }
+    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellCmd }
 
-    Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command $script" -Verb RunAs
+    # Use a local script to work around arguments issues.
+    if ($MyInvocation.MyCommand.Definition) {
+        # Create the path to the downloaded script file.
+        $scriptLocation = Join-Path "$env:TEMP" "winutil.ps1"
 
+        # Download the script to the '$env:TEMP' folder.
+        $OriginalProgressPreference = $ProgressPreference
+        $ProgressPreference = "SilentlyContinue"
+        Invoke-WebRequest -Uri $latestScriptURL -OutFile $scriptLocation
+        $ProgressPreference = $OriginalProgressPreference
+
+        # Start a new script instance with elevated privileges.
+        Start-Process $processCmd -ArgumentList "$powershellCmd -ExecutionPolicy Bypass -NoProfile -File $scriptLocation $argsList" -Verb RunAs
+        break
+    }
+
+    # Start a new script instance with elevated privileges.
+    Start-Process $processCmd -ArgumentList "$powershellCmd -ExecutionPolicy Bypass -NoProfile -Command $script $argsList" -Verb RunAs
     break
 }
 
+# Start WinUtil transcript logging.
 $dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-
 $logdir = "$env:localappdata\winutil\logs"
 [System.IO.Directory]::CreateDirectory("$logdir") | Out-Null
 Start-Transcript -Path "$logdir\winutil_$dateTime.log" -Append -NoClobber | Out-Null
 
-# Set PowerShell window title
-$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Admin)"
-clear-host
+# Set the fallback PowerShell window title.
+$fallbackWindowTitle = "WinUtil"
+
+# Set the PowerShell window title.
+try {
+    if ($MyInvocation.MyCommand.Path) {
+        $Host.UI.RawUI.WindowTitle = "(Admin) " + $MyInvocation.MyCommand.Path
+    } else {
+        $Host.UI.RawUI.WindowTitle = "(Admin) " + $MyInvocation.MyCommand.Definition
+    }
+} catch {
+    Write-Host "Exception setting `"WindowTitle`": Window title is too long. Using fallback window title." -ForegroundColor Yellow
+    $Host.UI.RawUI.WindowTitle = "(Admin) " + $fallbackWindowTitle
+}
